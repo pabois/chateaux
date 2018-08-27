be29e9ed223237d577da80295e4da1df3a5eb9ef
paboissinot <paboissinot@lespoupees.paris>
2018-08-27 10:59:10 +0200

    internal nav

--- app/controllers/chateaux_controller.rb
+++ app/controllers/chateaux_controller.rb
@@ -6,6 +6,14 @@ class ChateauxController < ApplicationController
 
   def show
     @chateau = Chateau.find(params[:id])
+
+    chateaux = Chateau.ordered
+    current_index = 0
+    chateaux.each.with_index do |chateau, index|
+      current_index = index if chateau.id = params[:id]
+    end
+    @prev = current_index < chateaux.length ? chateaux[current_index + 1] : nil
+    @next = current_index > 0 ? chateaux[current_index - 1] : nil
   end
 
 end
