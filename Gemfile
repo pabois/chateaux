b70c88d7d4c67937cdc568ef401ef5a96659cfe4
pabois <pierreandre.boissinot@noesya.coop>
2024-10-07 11:37:07 +0200

    update all

--- Gemfile
+++ Gemfile
@@ -1,25 +1,26 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.3'
+ruby '3.3.5'
 
-gem 'rails'
+gem "aws-sdk-s3", require: false
+gem 'bootstrap-datepicker-rails'
+gem 'bootstrap-sass'
+gem 'breadcrumbs_on_rails'                                  # Breadcrumbs
+gem 'font-awesome-sass'
+gem 'image_processing'
+gem 'jquery-rails'
+gem "nested_form"
+gem 'owlcarousel-rails'
 gem 'pg'
 gem 'puma'
+gem 'rails'
 gem 'sass-rails'
-gem 'uglifier'
-gem 'image_processing'
-gem 'bootstrap-sass'
-gem 'font-awesome-sass'
-gem 'owlcarousel-rails'
-gem 'jquery-rails'
 gem 'simple_form'
 gem 'simple-navigation'                                     # Nav                             https://github.com/codeplant/simple-navigation/
-gem 'breadcrumbs_on_rails'                                  # Breadcrumbs
-gem 'bootstrap-datepicker-rails'
-gem "aws-sdk-s3", require: false
+gem "sprockets-rails"
 gem 'toastr-rails'
-gem "nested_form"
+gem 'uglifier'
 
 group :development do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -28,3 +29,4 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen'
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_153622) do
-
+ActiveRecord::Schema[7.2].define(version: 2024_10_07_093029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +19,7 @@ ActiveRecord::Schema.define(version: 2018_08_23_153622) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -31,15 +30,22 @@ ActiveRecord::Schema.define(version: 2018_08_23_153622) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: nil, null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "chateaux", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "gallery_images", force: :cascade do |t|
@@ -48,4 +54,5 @@ ActiveRecord::Schema.define(version: 2018_08_23_153622) do
     t.index ["chateau_id"], name: "index_gallery_images_on_chateau_id"
   end
 
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
